068d8c72b917f94ac95b7f408c9e6f6d14b44780
laytn <pwh7121@gmail.com>
2023-04-10 21:16:23 +0900

    static폴더 생성

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 import uvicorn
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def read_root(request: Request):
